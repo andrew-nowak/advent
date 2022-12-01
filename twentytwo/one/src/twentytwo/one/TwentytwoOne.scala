296e4c327f3e0ca2ae0477badaa9cebc0ec69773
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-12-01 07:45:31 +0000

    2022: day 1

--- twentytwo/one/src/twentytwo/one/TwentytwoOne.scala
+++ twentytwo/one/src/twentytwo/one/TwentytwoOne.scala
@@ -3,7 +3,16 @@ package twentytwo.one
 import lib.Support
 
 object TwentytwoOne extends App with Support {
-  val i = load
+  val i = loadStringSeq
 
-  println(i)
+  val rankedElves: List[Int] = i.foldLeft((List.empty[Int], 0))((acc, cals) => {
+    if (cals.isEmpty) {
+      (acc._1 :+ acc._2, 0)
+    } else {
+      (acc._1, acc._2 + cals.toInt)
+    }
+  })._1
+
+  println(rankedElves.max)
+  println(rankedElves.sorted.takeRight(3).sum)
 }
