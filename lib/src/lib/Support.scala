b93c1e9e3b43d594cd1984ea43c83146db66f816
Andrew Nowak <andrew@nowak.me.uk>
2022-12-03 14:14:12 +0000

    new code skeleton: autofetch input and run on test data

--- lib/src/lib/Support.scala
+++ lib/src/lib/Support.scala
@@ -10,29 +10,38 @@ trait Support {
 
   def load: String = loadDirty.trim
 
+  def stringSeq(input: String, delimiter: String): Seq[String] =
+    input.split(delimiter)
   def loadStringSeq(delimiter: String): Seq[String] =
-    load.split(delimiter)
+    stringSeq(load, delimiter)
 
-  def loadStringSeq: Seq[String] = loadStringSeq(newline)
+  def stringSeq(input: String): Seq[String] = stringSeq(input, newline)
+  def loadStringSeq: Seq[String] = stringSeq(load)
 
-  def loadIntSeq(delimiter: String = newline): Seq[Int] =
-    load
+  def intSeq(input: String, delimiter: String = newline): Seq[Int] =
+    input
       .split(delimiter)
       .filter(_ != "")
       .map(_.toInt)
-
+  def loadIntSeq(delimiter: String = newline): Seq[Int] =
+    intSeq(load, delimiter)
+  @deprecated
   def loadIntSeq: Seq[Int] = loadIntSeq()
 
-  def load2dIntSeq(delimiterA: String = newline, delimiterB: String = " "): Seq[Seq[Int]] =
-    load
+  def `2dIntSeq`(input: String, delimiterA: String = newline, delimiterB: String = " "): Seq[Seq[Int]] =
+    input
       .split(delimiterA)
       .filter(_ != "")
       .map(_.split(delimiterB).map(_.toInt).toSeq)
+  def load2dIntSeq(delimiterA: String = newline, delimiterB: String = " "): Seq[Seq[Int]] =
+    `2dIntSeq`(load, delimiterA, delimiterB)
+  @deprecated def load2dIntSeq: Seq[Seq[Int]] = load2dIntSeq()
 
-  def load2dIntSeq: Seq[Seq[Int]] = load2dIntSeq()
-
-  def load2dIntSeqWithCoords(delimiterA: String = newline, delimiterB: String = " "): Map[Coord, Int] =
-    load2dIntSeq(delimiterA, delimiterB).zipWithIndex.flatMap { case (row, y) =>
+  def `2dIntSeqWithCoords`(input: String, delimiterA: String = newline, delimiterB: String = " "): Map[Coord, Int] =
+    `2dIntSeq`(input, delimiterA, delimiterB).zipWithIndex.flatMap { case (row, y) =>
       row.zipWithIndex.map { case (i, x) => Coord(x, y) -> i }
     }.toMap
+  def load2dIntSeqWithCoords(delimiterA: String = newline, delimiterB: String = " "): Map[Coord, Int] =
+    `2dIntSeqWithCoords`(load, delimiterA, delimiterB)
+
 }
