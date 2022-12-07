407db33e72780e5f0ab2778d26f47b664ac5f595
Andrew Nowak <andrew@nowak.me.uk>
2022-12-07 10:19:43 +0000

    2022: 7 (flattened, immutable)

--- twentytwo/seven/src/twentytwo/seven/TwentytwoSeven.scala
+++ twentytwo/seven/src/twentytwo/seven/TwentytwoSeven.scala
@@ -2,59 +2,42 @@ package twentytwo.seven
 
 import lib.Support
 
-import scala.collection.mutable
-
-case class File(name: String, size: Long)
-case class Directory(name: String, subdirs: mutable.ListBuffer[Directory], contents: mutable.ListBuffer[File]) {
-  def size: Long = contents.map(_.size).sum + subdirs.map(_.size).sum
-  def undersize: Long =
-    (if (size <= 100000) size else 0L) + subdirs.map(_.undersize).sum
-
-  def bestchoice(to: Long): Option[Long] =
-    (subdirs.map(_.bestchoice(to)) :+ (if (size >= to) Some(size) else None))
-      .flatten.minOption
-}
-case class State(loc: String, currentDirectory: Directory, parents: Seq[Directory])
 object TwentytwoSeven extends App with Support {
   val input = load
+
+  case class State(cwd: Seq[String], dirs: Map[String, Int]) {
+    def pwds: Seq[String] = "/" +: cwd.indices.map(n => cwd.take(n + 1)).map(_.mkString("/", "/", ""))
+  }
 
   def run(data: String) = {
     val in = stringSeq(data)
 
-    val root = Directory("/", mutable.ListBuffer.empty, mutable.ListBuffer.empty)
+    val initial = State(Seq(), Map.empty)
 
-    val initial = State("/", root, Seq.empty)
-
-    val p1 = in.tail.foldLeft(initial)((fs, outp) => {
-      if (outp == "$ ls") {
-        fs
-      } else if (outp == "$ cd ..") {
-        fs.copy(currentDirectory = fs.parents.head, parents = fs.parents.tail)
-      } else if (outp startsWith "$ cd ") {
-        fs.copy(currentDirectory =
-          fs.currentDirectory.subdirs.find(_.name == outp.drop(5)).get,
-          parents = fs.currentDirectory +: fs.parents
-        )
-      } else if (outp startsWith "dir ") {
-        val d = Directory(outp.drop(4), mutable.ListBuffer.empty, mutable.ListBuffer.empty)
-        fs.currentDirectory.subdirs.addOne(d)
-        fs
-      } else { // file
-        val Array(size, fname) = outp.split(" ")
-        val f = File(fname, size.toLong)
-        fs.currentDirectory.contents.addOne(f)
-        fs
+    val cdUp = """\$ cd ..""".r
+    val cd = """\$ cd (.+)""".r
+    val file = """(\d+) (.+)""".r
+    val tree = in.tail.foldLeft(initial)((fs, instr) => {
+      instr match {
+        case cdUp()  => State(fs.cwd.dropRight(1), fs.dirs)
+        case cd(dir) => State(fs.cwd :+ dir, fs.dirs)
+        case file(size, _) =>
+          fs.copy(dirs =
+            fs.pwds.foldLeft(fs.dirs)((dirMap, dirname) =>
+              dirMap.updatedWith(dirname)(_.map(_ + size.toInt).orElse(Some(size.toInt)))
+            )
+          )
+        case _ => fs // $ls and dir entries do nothing
       }
     })
 
-    val r = p1.parents.last
-    println(p1.parents.last.undersize)
+    val p1 = tree.dirs.values.filter(_ <= 100000).sum
+    println(p1)
 
-    val avail = 70000000 - r.size
-    val toreclaim = 30000000 - avail
-    println(r.bestchoice(toreclaim))
+    val tot = 30000000 - (70000000 - tree.dirs("/"))
+    val p2 = tree.dirs.values.filter(_ >= tot).min
+    println(p2)
   }
 
-  println("--- real ---")
   run(input)
 }
