63152de2bd10828d59b23f52417bf6ff8abeb71a
Andrew Nowak <andrew@nowak.me.uk>
2023-04-07 11:45:45 +0100

    2019: 16 rewrite to scala

--- nineteen/sixteen/src/nineteen/sixteen/NineteenSixteen.scala
+++ nineteen/sixteen/src/nineteen/sixteen/NineteenSixteen.scala
@@ -10,23 +10,38 @@ object NineteenSixteen extends App with Support {
       |""".stripMargin.trim
   val input = load
 
-  @tailrec def runFFT(signal: Array[Int], times: Int): Array[Int] = {
+  @tailrec
+  def runFFT(signal: Seq[Int], times: Int): Seq[Int] = {
     if (times == 0) signal
-    else runFFT(NineteenSixteenImpl.phase(signal), times - 1)
+    else runFFT(phase(signal), times - 1)
   }
 
-  @tailrec def runp2(signal: List[Int], times: Int): List[Int] = {
+  @tailrec
+  def runp2(signal: Seq[Int], times: Int): Seq[Int] = {
     if (times == 0) signal
     else runp2(signal.scanRight(0)((a, b) => (a + b) % 10).dropRight(1), times - 1)
+  }
+
+  def phase(signal: Seq[Int]): Seq[Int] = {
+    signal.indices.map { i =>
+      val value = for (k <- i until signal.length) yield {
+        ((k + 1) / (i + 1)) % 4 match {
+          case 1 => signal(k)
+          case 3 => -signal(k)
+          case _ => 0
+        }
+      }
+      Math.abs(value.sum) % 10
+    }
   }
 
   def run(data: String) = {
     val in = intSeq(data, delimiter = "")
 
-    println(runFFT(in.toArray, times = 100).take(8).mkString)
+    println(runFFT(in, times = 100).take(8).mkString)
 
     val reps = 10000
-    val inP2 = List.fill(reps)(in).flatten
+    val inP2 = Seq.fill(reps)(in).flatten
     val skip = in.take(7).mkString.toInt
     val tot = runp2(inP2.drop(skip), 100)
     println(tot.take(8).mkString)
