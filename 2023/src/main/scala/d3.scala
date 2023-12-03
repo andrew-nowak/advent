2e4427860f55f75cf750dcc9125a5ded14725505
Andrew Nowak <andrew@nowak.me.uk>
2023-12-03 12:16:14 +0000

    2023 day 3 p2

--- 2023/src/main/scala/d3.scala
+++ 2023/src/main/scala/d3.scala
@@ -1,6 +1,8 @@
 import lib.{Coord, Support}
 
 object d3 extends App with Support {
+  private final case class PartNumber(n: Long, part: Coord)
+  private final case class State(seen: List[PartNumber] = Nil, current: String = "", part: Option[Coord] = None)
 
   val testData =
     """467..114..
@@ -25,25 +27,29 @@ object d3 extends App with Support {
       }
     }
     val partLocs = parts.map(_._1).toSet
+    val gearLocs = parts.filter(_._2 == '*').map(_._1).toSet
 
     val numbers = in.zipWithIndex.flatMap { case (row, y) =>
       // go through all the numbers, building up iteratively into acc. as you go through each char, check if any of the
       // neighbouring locations has a part, if so store into valid. if valid is still false when you move to a
       // non-digit char, then this isn't a part number and can be discarded
-      val (rowNumbers, maybeFinalNo, valid) = row.zipWithIndex
+      val State(rowNumbers, maybeFinalNo, valid) = row.zipWithIndex
         .map { case (c, x) => (Coord(x, y), c) }
-        .foldLeft((List.empty[String], "", false)) { case ((seen, acc, validLoc), (loc, c)) =>
-          if (c.isDigit) (seen, acc + c, validLoc || partLocs.intersect(loc.neighbours.toSet).nonEmpty)
-          else if (acc.nonEmpty && validLoc) (seen :+ acc, "", false)
-          else (seen, "", false)
+        .foldLeft(State()) { case (State(seen, acc, validLoc), (loc, c)) =>
+          if (c.isDigit) State(seen, acc + c, validLoc orElse partLocs.intersect(loc.neighbours.toSet).headOption)
+          else if (acc.nonEmpty && validLoc.isDefined) State(seen :+ PartNumber(acc.toLong, validLoc.get), "", None)
+          else State(seen, "", None)
         }
-      val allRowNumbers = (if (valid) rowNumbers :+ maybeFinalNo else rowNumbers).map(_.toLong)
+      val allRowNumbers =
+        if (valid.isDefined) rowNumbers :+ PartNumber(maybeFinalNo.toLong, valid.get) else rowNumbers
       allRowNumbers
     }
 
-    lazy val p1 = numbers.sum
+    val numbersByPart = numbers.groupMap(_.part)(_.n)
 
-    lazy val p2 = in.size
+    lazy val p1 = numbers.map(_.n).sum
+
+    lazy val p2 = gearLocs.toList.map(loc => numbersByPart(loc)).filter(_.size == 2).map(_.product).sum
 
     println(p1)
 
