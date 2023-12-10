76ba2988d6d6755ddfaef299672a95ba7c6ee634
Andrew Nowak <andrew@nowak.me.uk>
2023-12-10 16:03:31 +0000

    2023 day 10 commentary

--- 2023/src/main/scala/d10.scala
+++ 2023/src/main/scala/d10.scala
@@ -53,12 +53,26 @@ object d10 extends App with Support {
 
     lazy val p1 = theLoop.size / 2
 
-    val zs = Seq(Set('F', 'J'), Set('7', 'L'))
+    val twists = Seq(Set('F', 'J'), Set('7', 'L'))
 
     lazy val p2 = {
       val space = map.keySet.diff(theLoop)
 
       space.filter(location => {
+        // how to know if you are "inside" the pipe?
+        // to be inside, there must be a non-even number of pipe "stretches" in each cardinal direction
+        // what is a pipe stretch? each perpendicular char (eg. '|' when looking west) is 1 stretch,
+        // but there can be multiple parallel chars (eg. '----' would be one stretch)
+        // parallel stretches must be formed of two turns with any number of straights inside, e.g 'F----J',
+        // so we can ignore the parallel straights, and just count the pairs of turns.
+        // but not all turn pairs count! a stretch like 'L----J' doesn't count because if that's all between
+        // you and the outside, you're still outside! only 'FJ' and 'L7' pairs count, because they're
+        // twists, rather than U-bends.
+        // also, you only need to know about north and west - because pipes can't cross, you know that if
+        // you appear to be inside by looking west, then you'll also appear inside when looking east
+        // (the number of stretches of pipe on any row or column MUST be even, so if there's an odd number
+        // west then there'll be an odd number east, etc.etc.)
+
         val (leftStraight, leftTurn) = (0 until location.x)
           .map(Coord(_, location.y))
           .filter(theLoop.contains)
@@ -70,7 +84,7 @@ object d10 extends App with Support {
         val left = leftStraight.size + leftTurn
           .grouped(2)
           .map(_.toSet)
-          .count(zs.contains)
+          .count(twists.contains)
 
         val (upStraight, upTurn) = (0 until location.y)
           .map(Coord(location.x, _))
@@ -80,11 +94,15 @@ object d10 extends App with Support {
           .filterNot(_ == '|')
           .partition(_ == '-')
         val up =
-          upStraight.size + upTurn.grouped(2).map(_.toSet).count(zs.contains)
+          upStraight.size + upTurn
+            .grouped(2)
+            .map(_.toSet)
+            .count(twists.contains)
 
         Seq(left, up).forall(n => n % 2 != 0)
       })
     }
+
     val dbgmap = map.filter(theLoop contains _._1) ++ p2.map(c => c -> 'X')
 
     val pretty = showCoords[Char](
